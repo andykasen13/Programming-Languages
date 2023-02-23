a31b389b768173236c7186f8ab0f1fbfb47a279a
andykasen13 <andrewgarcha@westminster.net>
2023-02-23 11:38:57 -0500

    Update MarioKart.java

--- My Language/MarioKart.java
+++ My Language/MarioKart.java
@@ -1,3 +1,37 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import LexicalAnalysis.Lexer;
+import LexicalAnalysis.Lexeme;
+
 public class MarioKart {
-    
+    public static void main(String[] args) throws IOException{
+        try {
+            if(args.length == 1) runFile(args[0]);
+            else { 
+                System.out.println("Usage: MarioKart [path to .arrg file]");
+                System.exit(64);
+            }
+        } catch (IOException exception) {
+            throw new IOException(exception.toString());
+        }
+    }
+
+    private static String getSourceCodeFromFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        return new String(bytes, Charset.defaultCharset());
+    }
+
+    private static void runFile(String path) throws IOException {
+        System.out.println("Running" + path + "...");
+        String source = getSourceCodeFromFile(path);
+
+        //Lexing
+        Lexer lexer = new Lexer(source);
+        ArrayList<Lexeme> lexemes = lexer.lex();
+        System.out.println(lexemes);
+    }
 }
